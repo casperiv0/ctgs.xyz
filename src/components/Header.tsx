b46a4b4e38eb99f9f3c3d1f481ed6d148cf31130
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-09-29 07:02:30 +0200

    :tada: feat: accounts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
+import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
 import { Moon, Sun } from "react-bootstrap-icons";
 import { getThemeFromLocal, Theme, updateBodyClass, updateLocalTheme } from "lib/theme";
 import { Button } from "./Button";
 
 export const Header = () => {
   const [theme, setTheme] = React.useState<Theme>("dark");
+  const session = useSession();
+
+  console.log(session);
 
   React.useEffect(() => {
     const t = getThemeFromLocal();
@@ -20,6 +25,13 @@ export const Header = () => {
     updateLocalTheme(newTheme);
   }
 
+  async function handleAuth() {
+    await signIn("github", {
+      redirect: false,
+      callbackUrl: "/",
+    });
+  }
+
   return (
     <>
       <div className="absolute top-5 left-5">
@@ -33,6 +45,18 @@ export const Header = () => {
           </a>
         </Button>
       </div>
+
+      {session.status === "authenticated" ? (
+        <Button className="absolute top-5 right-20 h-10">
+          <Link href="/account">
+            <a>Account</a>
+          </Link>
+        </Button>
+      ) : (
+        <Button onClick={handleAuth} className="absolute top-5 right-20 h-10">
+          Login
+        </Button>
+      )}
 
       <div className="absolute top-5 right-5">
         <Button
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { Footer } from "components/Footer";
 import { Header } from "components/Header";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider>
       <Header />
 
       <Component {...pageProps} />
 
       <Footer />
-    </>
+    </SessionProvider>
   );
 }
